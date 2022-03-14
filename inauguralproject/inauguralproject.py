6e584eda2aa6d05072504ccddf36a146a8a73117
laura <pqx792@alumni.ku.dk>
2022-03-14 11:19:18 +0100

    Lau test

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -13,3 +13,10 @@ def square(x):
     
     y = x**2
     return y
+
+
+def utility_function(z,t):
+    return (z ** (1 - t))/(1 - t)
+
+def insured_expenditures(q):
+        return p((y - x + q[0] - (p * q[0])) ** (1 + vartheta)) / (1 + vartheta) + (1-p)((y - (p * q[0])) ** (1 + vartheta)) / (1 + vartheta)
