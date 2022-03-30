78f148f0b7d12e5f24aa80f212de28a063bb7d8a
frejakatharina <99349449+frejakatharina@users.noreply.github.com>
2022-03-30 10:27:28 +0200

    Fixed identation error

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -4,15 +4,14 @@ from scipy import optimize
 
 # We are defining the model:
 
-    # Given parameters in the assignment:
-    y = 1
-    p = 0.2
-    theta = -2
+# Given parameters in the assignment:
+y = 1
+p = 0.2
+theta = -2
 
-    # Utility function
-    def u_func(z, theta):
-        
-        """ Agents utility of asset:
+# Utility function
+def u_func(z, theta):
+    """ Agents utility of asset:
             
             Args:
                 z (input array): input to utility function.
@@ -20,14 +19,13 @@ from scipy import optimize
                 
             Returns:
                 u (float): utility for given input. 
-        """
-        return (z ** (1 + theta))/(1 + theta)
+    """
+    return (z ** (1 + theta))/(1 + theta)
 
-    # Value function for both insured and uninsured:
+# Value function for both insured and uninsured:
 
-    def uninsured_value(x, y, p):
-
-        """ Expected utility for uninsured agents:
+def uninsured_value(x, y, p):
+    """ Expected utility for uninsured agents:
     
             Args:
                 y (float):  Total assets of agent. 
@@ -37,12 +35,11 @@ from scipy import optimize
         
             Returns:
                 Uninsured_value (float): Expected utility without insurance.
-        """
-        return p*u_func((y-x), theta) + (1 - p)*u_func(y, theta)
+    """
+    return p*u_func((y-x), theta) + (1 - p)*u_func(y, theta)
 
-    def insured_value(x, y, p, q):
-        
-        """ Expected utility for insured agents:
+def insured_value(x, y, p, q):
+    """ Expected utility for insured agents:
     
             Args:
                 y (float):  Total assets of agent. 
@@ -53,17 +50,16 @@ from scipy import optimize
         
             Returns:
                 Insured_value (float): Expected utility with insurance.
-        """
-        return p*u_func((y - x + q - p*q), theta)+(1 - p)*u_func((y - p*q), theta)
+    """
+    return p*u_func((y - x + q - p*q), theta)+(1 - p)*u_func((y - p*q), theta)
 
 # Question 1: 
 
     # i. We are defining an function, which returns the optimized coverage amount, q*. The coverage q* maximizes expected
     #    utility for the agents in the case, where the premium is just equal to the expected insurance payout, pi = q*p.
 
-    def optimal(x, y, p): 
-        
-            """
+def optimal(x, y, p): 
+    """
             Optimal coverage amount, q, for the given monetary loss, x, of the agents.
             
             Args:
@@ -74,16 +70,16 @@ from scipy import optimize
             Returns:
                   solution_q (array): Optimal coverage, q*, cannot be higher than the monetary loss. This means that the
                                       bounds for q are (0 - 0.9).
-            """
-        res = optimize.minimize_scalar(lambda q: -insured_value(x, y, p, q), method = 'bounded', bounds = (0,x))
-        return res
+    """
+    res = optimize.minimize_scalar(lambda q: -insured_value(x, y, p, q), method = 'bounded', bounds = (0,x))
+    return res
     
 # Question 2: 
 
     # i. We are defining value function with fixed pi: 
 
-    def value_pi(pi, q, x, y, p):   
-            """
+def value_pi(pi, q, x, y, p):   
+    """
             Expected utility value with fixed pi. 
             
             Args:
@@ -94,17 +90,16 @@ from scipy import optimize
         
             Returns: 
             Expected utility (float or array): Agent being insured with coverage q and the optimal premium pi.
-            """
-            return p*u_func((y - x + q - pi), theta)+(1 - p)*u_func((y - pi), theta)
+    """
+    return p*u_func((y - x + q - pi), theta)+(1 - p)*u_func((y - pi), theta)
 
     # ii. we guess the solution of pi_tilde, which is the premium that makes the expected utility of 
     # getting insurance equal to the utility from not getting insurance.
 
-    pi_guess = 0.01
+pi_guess = 0.01
 
-    def optimal_pi(q, x, y, p, V0):
-        
-            """
+def optimal_pi(q, x, y, p, V0):
+    """
             Maximizing premium - solving for the optimal pi:
         
             Args:
@@ -116,26 +111,25 @@ from scipy import optimize
             Returns: 
             result_pi_optimal (float or array): 
         
-            """
+    """
     # We are defining the objective function and optimizing.
-        def obj(pi):
-            return value_pi(pi, q, x, y, p) - V0
+    def obj(pi):
+        return value_pi(pi, q, x, y, p) - V0
 
     # Solving for the expected value to be V = V0    
-        res = optimize.root(obj, pi_guess, method='hybr')
-        return  res
+    res = optimize.root(obj, pi_guess, method='hybr')
+    return  res
     
 # Question 3: 
     
     # i. Defining monte carlo function by setting seed number and parameters.
-    np.random.seed(123)
-    N = 10000
-    alpha = 2
-    beta = 7
+np.random.seed(123)
+N = 10000
+alpha = 2
+beta = 7
 
-    def agent_value(x, y, p, gamma, pi):
-        
-        """
+def agent_value(x, y, p, gamma, pi):
+    """
         A modification of previous function for expected utility, which calculates the
         agent's expected utility, when the loss, x, is drawn from a beta distribution and a fraction, gamma, is covered. 
         
@@ -147,11 +141,10 @@ from scipy import optimize
             gamma (float): Fraction of loss that is covered by insurance. 
             
         Returns: Utility for given gamma and pi
-        """
-        return u_func((y - (1 - gamma)*x - pi), theta)
+    """
+    return u_func((y - (1 - gamma)*x - pi), theta)
 
-    def monte_carlo(y, p, gamma, pi, N):
-        
+def monte_carlo(y, p, gamma, pi, N):
     """
         Function that calculates difference in expected utility from having the full loss 
         covered and only a fraction, gamma, covered.
@@ -164,18 +157,17 @@ from scipy import optimize
         
         Returns:  
     """
-        xs = np.random.beta(alpha, beta, N) # draws N number of xs from a beta distribution
-        return np.mean(agent_value(xs, y, p, gamma, pi)) # return average
+    xs = np.random.beta(alpha, beta, N) # draws N number of xs from a beta distribution
+    return np.mean(agent_value(xs, y, p, gamma, pi)) # return average
     
 # Question 4: 
 
     # We begin with setting the interial guess for the solution. 
     
-    pi_guess_c = 0.01
+pi_guess_c = 0.01
 
-    def optimal_pi_c(y, p, gamma, pi, N, U0):
-        
-        """
+def optimal_pi_c(y, p, gamma, pi, N, U0):
+    """
         Calculates the profit maximizing premium when insurer acts as a monopolist,
         the client's loss is stochastic and coverage is proportional to the loss.
         
@@ -184,9 +176,8 @@ from scipy import optimize
             x (array): Distribution of losses.
         
             Returns:  
-        """
-        def U(pi):
-            
+    """
+    def U(pi):
         """
             This function minizimses the difference between the utility for insured and uninsured agents.  
         
@@ -197,9 +188,8 @@ from scipy import optimize
         Returns:  
             Solution_utility 
         """
-            return monte_carlo(y, p, gamma, pi, N) - U0
+        return monte_carlo(y, p, gamma, pi, N) - U0
         
         # solving the utility for insured agents being equal to uninsured agents. 
-        sol = optimize.root(U, pi_guess_c, method = 'broyden1')
-        
-        return sol
+    sol = optimize.root(U, pi_guess_c, method = 'broyden1')
+    return sol
